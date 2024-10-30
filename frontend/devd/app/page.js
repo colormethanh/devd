52fb14adb612c336e697531e791b5e9c62f82598
Colormethanh <thnguyen1847@gmail.com>
2024-10-30 17:19:59 -0400

    added next router

--- frontend/devd/app/page.js
+++ frontend/devd/app/page.js
@@ -1,7 +1,10 @@
 "use client";
 import Button from "./components/utilities/Button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col h-full w-100 text-center justify-center">
       <div className="">
@@ -11,14 +14,15 @@ export default function Home() {
       <div className="home-options flex flex-col items-center mt-3">
         <Button
           clickCallback={() => {
-            console.log("button clicked");
+            console.log("going to projects page clicked");
           }}
         >
           Enter as guest
         </Button>
         <Button
           clickCallback={() => {
-            console.log("button clicked");
+            console.log("Going to Login page clicked");
+            router.push("/auth");
           }}
           addStyle="mt-3"
         >
@@ -26,7 +30,7 @@ export default function Home() {
         </Button>
         <Button
           clickCallback={() => {
-            console.log("button clicked");
+            console.log("Going to signup page clicked");
           }}
           addStyle="mt-3"
         >
--- frontend/devd/app/store/slices/authSlice.js
+++ frontend/devd/app/store/slices/authSlice.js
@@ -1,8 +1,4 @@
-import {
-  createSlice,
-  createAsyncThunk,
-  isRejectedWithValue,
-} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3000";
