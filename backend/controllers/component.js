6d67e6b4cbe9615338ccf9c0d2121bc522db8ced
Colormethanh <thnguyen1847@gmail.com>
2024-11-13 23:52:35 -0500

    fixed capitalization error

--- backend/controllers/component.js
+++ backend/controllers/component.js
@@ -1,5 +1,5 @@
 const ComponentModel = require("../models/Components");
-const ProjectModel = require("../models/Project");
+const ProjectModel = require("../models/project");
 const PageModel = require("../models/Page");
 const { createError } = require("../utils/errorHelpers");
 const logger = require("../utils/logging/logger");
