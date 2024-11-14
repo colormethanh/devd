a478a53030a1eb15c4850788da142061281cd870
Colormethanh <thnguyen1847@gmail.com>
2024-11-13 23:34:59 -0500

    updated git ignore

--- backend/config/dev.js
+++
@@ -1,8 +0,0 @@
-// configs for dev
-module.exports = {
-  MONGO_URI: "mongodb://localhost/devd",
-  TOKEN_SECRET: "ToTheOldLady",
-  CLOUDINARY_CLOUD_NAME: "dlbs4lkhx",
-  CLOUDINARY_API_KEY: "118934653613373",
-  CLOUDINARY_SECRET_KEY: "ma2kqQVqfZk7EcjRZXbRv2RFRmQ",
-};
